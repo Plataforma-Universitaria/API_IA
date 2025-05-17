d592d46cc868a517f4f4779b58fceb9eacdacf30
S4R4GDL <saragdl7321@gmail.com>
2025-05-17 13:40:16 -0300

    Atualizando dependencias e métodos

--- src/main/java/br/ueg/tc/apiai/enums/AiSpec.java
+++
@@ -1,8 +0,0 @@
-package br.ueg.tc.apiai.enums;
-
-public enum AiSpec {
-    OPENAI_OMINI_4("gpt-4");
-
-    AiSpec(String version) {
-    }
-}
--- src/main/java/br/ueg/tc/apiai/service/AiService.java
+++ src/main/java/br/ueg/tc/apiai/service/AiService.java
@@ -3,8 +3,15 @@ package br.ueg.tc.apiai.service;
 import br.ueg.tc.apiai.contract.client.AbstractClient;
 import br.ueg.tc.apiai.contract.client.ChatClientFactory;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.ai.openai.OpenAiChatOptions;
+import org.springframework.ai.openai.api.ResponseFormat;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AiService<C extends AbstractClient> {
@@ -13,6 +20,21 @@ public class AiService<C extends AbstractClient> {
     public AiService(ChatClientFactory<C> chatClientFactory) {
         OpenAiChatModel openAiChatModel = chatClientFactory.createChatModel();
         this.chatClient = ChatClient.create(openAiChatModel);
+    }
+
+    public ChatResponse sendPrompt(String promptText, double temperature, Integer maxTokens, ResponseFormat responseFormat) {
+        Prompt prompt = new Prompt(List.of(new UserMessage(promptText)));
+
+        OpenAiChatOptions options = OpenAiChatOptions.builder()
+                .temperature(temperature)
+                .maxTokens(maxTokens)
+                .responseFormat(responseFormat != null ? responseFormat : new ResponseFormat(ResponseFormat.Type.TEXT, "text"))
+                .build();
+
+        return (ChatResponse) this.chatClient.prompt()
+                .options(options)
+                .messages(prompt.getInstructions())
+                .call();
     }
 
     public String sendPrompt(String prompt) {
